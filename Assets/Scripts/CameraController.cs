8edd29cf3058e679df5ca960a290e959856a45f4
Zakareya Alatoli <ghetgeek3@aol.com>
2018-08-23 22:41:49 -0400

    Camera and Movement
    -Added basic movement (walking and jumping)
    
    -Added camera that tracks and moves with the player

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    //If false, the camera will be offset by its 
+    //relative position to the target in the scene, rather
+    //than the offset entered into the script itself
+    public bool useOffsetValues;
+    public Transform target;
+    //How far the camera should be from the world
+    public Vector3 offset;
+    // Use this for initialization
+
+	void Start () {
+        if(!useOffsetValues)
+            offset = target.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = target.position - offset;
+        transform.LookAt(target);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public CharacterController controller;
+    public float moveSpeed;
+    public float jumpForce;
+    public float terminalVelocity;
+
+    public Vector3 moveDirection;
+    public float gravityScale;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
+        if (controller.isGrounded)
+        {
+            moveDirection.y = -1f;
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y = jumpForce;
+            }
+        }
+        else
+        {
+            moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale);
+            if (moveDirection.y > terminalVelocity)
+                moveDirection.y = terminalVelocity;
+        }
+        controller.Move(moveDirection * Time.deltaTime);
+
+
+    }
+}
