d857b10c8007bcb5d4d0073a4f311078e4100545
Zakareya Alatoli <ghetgeek3@aol.com>
2018-08-30 12:57:51 -0400

    Particle effects
    -Added particle effects to gold being picked up

--- Assets/Scripts/GoldPickup.cs
+++ Assets/Scripts/GoldPickup.cs
@@ -6,6 +6,8 @@ public class GoldPickup : MonoBehaviour {
 
     public int value;
 
+    public GameObject pickupEffect;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -20,6 +22,7 @@ public class GoldPickup : MonoBehaviour {
     {
         if(other.tag == "Player")
         {
+            Instantiate(pickupEffect, transform.position, transform.rotation);
             FindObjectOfType<GameManager>().AddGold(value);
             Destroy(gameObject);
         }
