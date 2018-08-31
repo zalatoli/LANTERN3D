e390a576f7a022ed0b06e12d4041d0ad6990e993
Zakareya Alatoli <ghetgeek3@aol.com>
2018-08-31 11:07:02 -0400

    KB, Damage, Invincibility, Respawn
    -Added the ability for the player to receive damage and knockback
    -Added mercy invincibility on hit along with flickering effect
    -Added the ability to respawn

---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour {
+
+    public int maxHealth;
+    public int currentHealth;
+
+    public PlayerController thePlayer;
+
+    //Invincibility upon taking damage
+    public float invincibilityLength;
+    private float invincibilityCounter;
+
+    public Renderer playerRenderer;
+    //count down until next flash state transition
+    private float flashCounter;
+    //Controls the length of transition between flashes
+    //when player takes damage
+    public float flashLength = 0.1f;
+
+    private bool isRespawning;
+    private Vector3 respawnPoint;
+
+    public float respawnLength;
+
+	// Use this for initialization
+	void Start () {
+        currentHealth = maxHealth;
+        //thePlayer = FindObjectOfType<PlayerController>();
+
+        respawnPoint = thePlayer.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(invincibilityCounter > 0)
+        {
+            invincibilityCounter -= Time.deltaTime;
+
+            //When the flash counter reaches zero the player
+            //will switch between visible/invisible until
+            //the invincibility ends
+            flashCounter -= Time.deltaTime;
+            if(flashCounter <= 0)
+            {
+                playerRenderer.enabled = !playerRenderer.enabled;
+                flashCounter = flashLength;
+            }
+            //Make sure the player is visible when 
+            //invincibility ends
+            if(invincibilityCounter <= 0)
+            {
+                playerRenderer.enabled = true;
+            }
+        }
+		
+	}
+    public void HurtPlayer(int damage, Vector3 direction)
+    {
+        if(invincibilityCounter <= 0)
+        {
+            currentHealth -= damage;
+
+            if(currentHealth <= 0)
+            {
+                Respawn();
+            }
+            else
+            {
+                thePlayer.Knockback(direction);
+                invincibilityCounter = invincibilityLength;
+                playerRenderer.enabled = false;
+                flashCounter = flashLength;
+            }
+
+        }
+    }
+
+    public void HealPlayer(int heal)
+    {
+        currentHealth += heal;
+        if (currentHealth > maxHealth)
+            currentHealth = maxHealth;
+    }
+
+    public void Respawn()
+    {
+
+        if (!isRespawning)
+        {
+            StartCoroutine("RespawnCo");
+        }
+        
+    }
+    //IEnumerator is a coroutine which operates on its
+    //own timeline
+    public IEnumerator RespawnCo()
+    {
+        isRespawning = true;
+        thePlayer.gameObject.SetActive(false);
+        yield return new WaitForSeconds(respawnLength);
+        isRespawning = false;
+
+        thePlayer.gameObject.SetActive(true);
+        thePlayer.transform.position = respawnPoint;
+        currentHealth = maxHealth;
+
+        invincibilityCounter = invincibilityLength;
+        playerRenderer.enabled = false;
+        flashCounter = flashLength;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,6 +19,10 @@ public class PlayerController : MonoBehaviour {
     public float rotateSpeed;
     public GameObject playerModel;
 
+    public float knockBackForce;
+    public float knockBackTime;
+    private float knockBackCounter;
+
     // Use this for initialization
     void Start () {
         controller = GetComponent<CharacterController>();
@@ -27,28 +31,35 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         //moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
-        float yStore = moveDirection.y;
-        moveDirection = (transform.forward * Input.GetAxisRaw("Vertical")) + (transform.right * Input.GetAxisRaw("Horizontal"));
-        moveDirection = moveDirection.normalized * moveSpeed;
-        moveDirection.y = yStore;
 
-        if (controller.isGrounded)
+
+        if (knockBackCounter <= 0)
         {
-            moveDirection.y = -1f;
-            if (Input.GetButtonDown("Jump"))
+            float yStore = moveDirection.y;
+            moveDirection = (transform.forward * Input.GetAxisRaw("Vertical")) + (transform.right * Input.GetAxisRaw("Horizontal"));
+            moveDirection = moveDirection.normalized * moveSpeed;
+            moveDirection.y = yStore;
+
+            if (controller.isGrounded)
             {
-                moveDirection.y = jumpForce;
+                moveDirection.y = -1f;
+                if (Input.GetButtonDown("Jump"))
+                {
+                    moveDirection.y = jumpForce;
+                }
             }
         }
         else
         {
-            moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale);
-            if (moveDirection.y > terminalVelocity)
-                moveDirection.y = terminalVelocity;
+            knockBackCounter -= Time.deltaTime;
         }
+
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale);
+        if (moveDirection.y > terminalVelocity)
+            moveDirection.y = terminalVelocity;
         controller.Move(moveDirection * Time.deltaTime);
 
-        
+
         //Move the player in different directions based on camera direction
         //If the player is trying to move in a direction rotate them to the 
         //pivot rotation
@@ -64,6 +75,14 @@ public class PlayerController : MonoBehaviour {
         //where normally the Get Axis functions return a range of values including negatives
         anim.SetFloat("Speed", (Mathf.Abs(Input.GetAxis("Vertical")) + Mathf.Abs(Input.GetAxis("Horizontal"))));
 
+
+    }
+
+    public void Knockback(Vector3 direction)
+    {
+        knockBackCounter = knockBackTime;
+        moveDirection = direction * knockBackForce;
+        moveDirection.y = knockBackForce;
 
     }
 }
